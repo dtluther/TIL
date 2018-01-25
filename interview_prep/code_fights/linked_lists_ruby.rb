211ac6f8d50b3f1342184c70ab60f93f43e6de73
Dillon <dillontluther@gmail.com>
2018-01-25 10:16:20 -0800

    finished removeKfromList

--- interview_prep/code_fights/linked_lists_ruby.rb
+++ interview_prep/code_fights/linked_lists_ruby.rb
@@ -16,6 +16,53 @@ end
 # from list l that have a value equal to k.
 
 def removeKFroList(l, k)
-    p1 =
+# First non-elegant solution
+#     return [] if l == nil
+    
+#     until l.value != k
+#         return [] if l.next == nil
+#         l = l.next
+#     end
+    
+#     p1 = l
+#     p2 = l.next
+    
+#     until p2 == nil
+#         unless p2.value == k
+#             p1.next = p2
+#             p1 = p1.next
+#         end  
+        
+#         p2 = p2.next
+#     end
+    
+#     p1.next = nil
+   
+#     l
+
+# A more elegant solution -- My preferred
+#     while l and l.value == k
+#         l = l.next
+#     end
+    
+#     curr = l
+#     while curr && curr.next
+#         if curr.next.value == k
+#             curr.next = curr.next.next
+#         else
+#             curr = curr.next
+#         end
+#     end
+    
+#     l
+
+# A elegant recursive solution
+#     if l.nil?
+#         return nil
+#     else
+#         l.next = removeKFromList(l.next, k)
+#         l.value == k ? l.next : l
+#     end
+
 
 end
