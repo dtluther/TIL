298bd1d91025a7971b73a086868272e9f7971236
Dillon Luther <lex@Dillons-MacBook-Air.local>
2020-03-23 23:58:04 -0700

    learnign about modules in ch 6

--- python_practice/test.py
+++ python_practice/test.py
@@ -398,18 +398,19 @@
 # d3 = dict(sape=4139, guido=4127, jack=4098)
 # print(d3)
 
-### 5.6 Looping Techniques
-## when looping through dicts, items() method retrives the key and corredpsonding value
-knights = {'gallahad': 'the pure', 'robin': 'the brave'}
-for k, v in knights.items():
-    print(k, v)
+# ### 5.6 Looping Techniques
+# ## when looping through dicts, items() method retrives the key and corredpsonding value
+# knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+# for k, v in knights.items():
+#     print(k, v)
 
-## when looping through a sequence, enumerate() gives position index and value
-for i, v in enumerate(['tic', 'tac', 'toe']):
-    print(i, v)
+# ## when looping through a sequence, enumerate() gives position index and value
+# for i, v in enumerate(['tic', 'tac', 'toe']):
+#     print(i, v)
 
-## to loop over 2+ sequences simultaneously, entries can be paired with zip()
-questions = ['name', 'quest', 'favorite color']
-answers = ['lancelot', 'the holy grail', 'blue']
-for q, a in zip(questions, answers):
-    print('What is your {0}? it is {1}.'.format(q,a))
+# ## to loop over 2+ sequences simultaneously, entries can be paired with zip()
+# questions = ['name', 'quest', 'favorite color']
+# answers = ['lancelot', 'the holy grail', 'blue']
+# for q, a in zip(questions, answers):
+#     print('What is your {0}? it is {1}.'.format(q,a))
+
