b99bbf1314a4cd67536a18bade26bcd98e7fd57d
Dillon Luther <lex@Dillons-MacBook-Air.local>
2020-03-25 00:06:06 -0700

    starting formatted string literals

--- python_practice/test.py
+++ python_practice/test.py
@@ -414,3 +414,37 @@
 # for q, a in zip(questions, answers):
 #     print('What is your {0}? it is {1}.'.format(q,a))
 
+#### 7 Input and Output ####
+### Fancier Output Formatting
+## formatted string literal
+# year = 2016
+# event = 'Referendum'
+# print(f'Results of the {year} {event}')
+
+# yes_votes = 42_572_654
+# no_votes = 43_132_495
+# percentage = yes_votes / (yes_votes + no_votes)
+# print('{:-9} YES votes  {:2.2%}'.format(yes_votes, percentage))
+
+# s = 'Hello, world.'
+# print(str(s))
+# print(repr(s))
+
+# print(str(1/7))
+
+# x = 10 * 3.25
+# y = 200 * 200
+# s = 'The value of x is ' + repr(x) + ', and y is ' +  repr(y) + '...'
+# print(s)
+
+# hello = 'hello, world\n'
+# hellos = repr(hello)
+# print(hellos)
+
+# # The argument to repr() may be any Python object:
+# print(repr((x, y, ('spam', 'eggs'))))
+
+### 7.1.1 Formatted String Literals
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 7678}
+for name, phone in table.items():
+    print(f'{name:10} ==> {phone:10d}')
