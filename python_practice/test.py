6e8d574cf54593efeff5461e0aa3c01097277396
Dillon Luther <dillon.luther@gmail.com>
2020-03-26 17:10:28 -0700

    quick notes on more try

--- python_practice/test.py
+++ python_practice/test.py
@@ -545,3 +545,31 @@
 #         print("D")
 #     except C:
 #         print("C")
+
+# import sys
+
+# try:
+#     f = open('myfile.txt')
+#     s = f.readline()
+#     i = int(s.strip())
+# except OSError as err:
+#     print("OS error: {0}".format(err))
+# except ValueError:
+#     print("Could not convert data to an integer.")
+# except:         # CAREFUL, this last open except serves as a wildcard, which can mask a real programming error
+#     print("Unexpected error:", sys.exc_info()[0])
+#     raise
+
+## There is an optional else clause, which must come last.
+## It is useful for code that must be executed if no exception is raised in the try clause
+## The else cause is better than adding to to the try clause because it avoids
+## accidentally catching an exception that wasn't raised by the code being protected
+## by the try... except statement.
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')
+        except OSError:
+            print('cannot open', arg)
+        else:
+            print(arg, 'has', len(f.readlines()), 'lines')
+            f.close()
