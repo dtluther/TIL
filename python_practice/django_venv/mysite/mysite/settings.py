7adf7870497d792bea9d1e0f3ee5e0093125830c
Dillon Luther <dillon.luther@gmail.com>
2020-04-06 14:44:34 -0700

    removing files and doinga  real virtualenv instead of venv for django

--- python_practice/django_venv/mysite/mysite/settings.py
+++ python_practice/django_venv/mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangotutorialdb',
+        'USER': 'django_tutorial',
+        'PASSWORD': 'test123',
+        'HOST': '',
+        'PORT': '5432'
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
