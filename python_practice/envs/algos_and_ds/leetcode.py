7a308fa8074a1d7685e8c2a7fec640d844b7523f
Dillon Luther <dillon.luther@gmail.com>
2020-04-15 23:51:23 -0700

    figuring out length of longest substring

--- python_practice/envs/algos_and_ds/leetcode.py
+++ python_practice/envs/algos_and_ds/leetcode.py
@@ -32,46 +32,74 @@ class ListNode:
 # print(l1.next.val)
 # print(l1.next.next.val)
 	
-class Solution:
-	def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-		num_arr_1 = []
-		num_arr_2 = []
-		curr1 = l1
-		curr2 = l2
+# class Solution:
+# 	def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+# 		num_arr_1 = []
+# 		num_arr_2 = []
+# 		curr1 = l1
+# 		curr2 = l2
+# 
+# 		while curr1:
+# 			num_arr_1.insert(0, str(curr1.val))
+# 			curr1 = curr1.next
+# 
+# 		while curr2:
+# 			num_arr_2.insert(0, str(curr2.val))
+# 			curr2 = curr2.next
+# 
+# 		num1 = int(''.join(num_arr_1))
+# 		num2 = int(''.join(num_arr_2))
+# 		total_str = str(num1 + num2)
+# 
+# 		i = len(total_str) - 1
+# 		l3 = ListNode(int(total_str[i]))
+# 		i = i - 1
+# 		curr3 = l3
+# 		while i >= 0:
+# 			curr3.next = ListNode(int(total_str[i]))
+# 			curr3 = curr3.next
+# 			i = i-1
+# 
+# 		return l3
+# 
+# x = Solution()
+# list1 = ListNode(2)
+# list1.next = ListNode(4)
+# list1.next.next = ListNode(3)
+# list2 = ListNode(5)
+# list2.next = ListNode(6)
+# list2.next.next = ListNode(4)
+# result = x.addTwoNumbers(list1, list2)
+# print(f"{result.val} -> {result.next.val} -> {result.next.next.val}")
 
-		while curr1:
-			num_arr_1.insert(0, str(curr1.val))
-			curr1 = curr1.next
+# 3) Longest Substring Without Repeating Characters
+def lengthOfLongestSubstring(s):
+	seen = {}
+	longest = 0
+	current = 0
+	for idx, char in enumerate(s):
+		if char in seen:
+			first = seen[char]
+			seen[char] = idx
+			if (idx-first) > longest:
+				current = idx - first
+			print(f"if: {seen}, {longest}, {current}")
+		else:
+			seen[char] = idx
+			current += 1
+			if current > longest:
+				longest = current
+			print(f"else: {seen}, {longest}, {current}")
 
-		while curr2:
-			num_arr_2.insert(0, str(curr2.val))
-			curr2 = curr2.next
+	return longest
 
-		num1 = int(''.join(num_arr_1))
-		num2 = int(''.join(num_arr_2))
-		total_str = str(num1 + num2)
+print(lengthOfLongestSubstring('abcabcbb'))
+print(lengthOfLongestSubstring('aab'))
+print(lengthOfLongestSubstring('dvdf'))
+print(lengthOfLongestSubstring('pwwkew'))
+print(lengthOfLongestSubstring('tmmzuxt'))
 
-		i = len(total_str) - 1
-		l3 = ListNode(int(total_str[i]))
-		i = i - 1
-		curr3 = l3
-		while i >= 0:
-			curr3.next = ListNode(int(total_str[i]))
-			curr3 = curr3.next
-			i = i-1
-
-		return l3
-
-x = Solution()
-list1 = ListNode(2)
-list1.next = ListNode(4)
-list1.next.next = ListNode(3)
-list2 = ListNode(5)
-list2.next = ListNode(6)
-list2.next.next = ListNode(4)
-result = x.addTwoNumbers(list1, list2)
-print(f"{result.val} -> {result.next.val} -> {result.next.next.val}")
-
+# 4) Median of Two Sorted Arrays
 
 # # From April Challenge
 # def singleNumber(nums):    
