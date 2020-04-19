31e7c7ff7bb8c8d6630b425ac4f2adbd8781b2a6
Dillon Luther <dillon.luther@gmail.com>
2020-04-18 23:50:38 -0700

    removed django venv, installed postgres succesfully

--- python_practice/django_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/lex/Documents/coding/til/python_practice/django_venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
