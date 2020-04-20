065fa7c4a3ef02567cbeb0866a02bb58f471a17e
Dillon Luther <dillon.luther@gmail.com>
2020-04-19 19:02:30 -0700

    began react postgres project, and started markdown file to keep track of what I did

---
+++ python_practice/envs/django_react_postgres/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/lex/Documents/coding/til/python_practice/envs/django_react_postgres/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
