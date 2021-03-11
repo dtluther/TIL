1a2a30e070ec6a9a88905b56cc48fc7dbb1c9f61
Dillon Luther <dillon.luther@gmail.com>
2021-03-11 11:07:24 -0800

    whitespace

--- design_patterns/factory.js
+++ design_patterns/factory.js
@@ -1,11 +1,11 @@
 function Developer(name) {
-	this.name = name;
-	this.type = "Developer";
+  this.name = name;
+  this.type = "Developer";
 }
 
 function Tester(name) {
-	this.name = name;
-	this.type = "Tester";
+  this.name = name;
+  this.type = "Tester";
 }
 
 function say() {
@@ -13,16 +13,16 @@ function say() {
 }
 
 function EmployeeFactory() {
-	this.create = (name, type) => {
-		switch(type) {
-			case 1:
-				return new Developer(name)
-				break;
-			case 2:
-				return new Tester(name)
-				break;
-		}
-	}
+  this.create = (name, type) => {
+    switch(type) {
+      case 1:
+        return new Developer(name)
+        break;
+      case 2:
+        return new Tester(name)
+        break;
+    }
+  }
 }
 
 const employees = []
@@ -32,5 +32,5 @@ employees.push(employeeFactory.create('Jane', 1));
 employees.push(employeeFactory.create('John', 2));
 
 employees.forEach(emp => {
-	say.call(emp);
+  say.call(emp);
 })
